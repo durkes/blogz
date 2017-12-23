9822489f79983ec69e5fe23f88b816d3764d7361
durkes <j@durkes>
2017-12-23 14:36:15 -0600

    Updated db model

--- main.py
+++ main.py
@@ -1,23 +1,44 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:demo@localhost:8889/blogz'
-# app.config['SQLALCHEMY_ECHO'] = True
-# db = SQLAlchemy(app)
-# app.secret_key = 'y737kGcys&zP3T'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:demo@localhost:8889/blogz'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = 'x737kJcyz&zP3T'
 
 
-# class Blog(db.Model):
-#
-#     id = db.Column(db.Integer, primary_key=True)
-#     title = db.Column(db.String(240))
-#     body = db.Column(db.String(2400))
-#
-#     def __init__(self, title, body):
-#         self.title = title
-#         self.body = body
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(240))
+    body = db.Column(db.String(2400))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __init__(self, title, body, owner):
+        self.title = title
+        self.body = body
+        self.owner = owner
+
+
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, email, password):
+        self.user = user
+        self.password = password
+
+
+@app.before_request
+def require_login():
+    allowed_routes = ['index', 'blog', 'login', 'signup']
+    if request.endpoint not in allowed_routes and 'user' not in session:
+        return redirect('/login')
 
 
 @app.route('/blog', methods=['GET'])
